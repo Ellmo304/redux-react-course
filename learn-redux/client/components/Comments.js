1373446b9be623f4e128cabbb5211eba43720faa
Elliot Brock <ell_304@msn.com>
2017-03-13 15:35:33 +0000

    lesson 15

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -11,11 +11,18 @@ return (
   </p>
   </div>
 )  },
+handleSubmit(e) {
+  e.preventDefault();
+  const postId = this.props.params.postId;
+  const author = this.ref.author.value;
+  const comment = this.ref.comment.value;
+  this.props.addComment(postId, author, comment);
+},
   render() {
     return (
       <div className='comments'>
       {this.props.postComments.map(this.renderComment)}
-      <form ref='commentForm' className='comment-form'>
+      <form onSubmit={this.handleSubmit} ref='commentForm' className='comment-form'>
       <input type='text' ref='author' placeholder='author'/>
       <input type='text' ref='comment' placeholder='comment'/>
       <input type='submit' hidden/>
